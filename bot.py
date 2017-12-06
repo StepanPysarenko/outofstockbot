f2c697254ae8e26c37ba131677d7dcf671e195e9
StepanPysarenko <stepan.pysarenko@gmail.com>
2017-12-06 23:05:14 +0200

    initial commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+import telebot
+import os
+from flask import Flask, request
+
+
+bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
+server = Flask(__name__)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route(os.environ.get('HOOK_URL'), methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=os.environ.get('HOOK_URL'))
+    return "!", 200
+
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
