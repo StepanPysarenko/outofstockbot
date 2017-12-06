9ca43db43c3751f7fe2d85ab42fee77f2fc354db
StepanPysarenko <stepan.pysarenko@gmail.com>
2017-12-07 00:39:18 +0200

    small improvements

--- bot.py
+++ bot.py
@@ -12,9 +12,19 @@ def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
+@bot.message_handler(commands=['help'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name + '. Need help?')
+
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['location'])
+def echo_message(message):
+    bot.reply_to(message, location.latitude + ', ' + location.longitude)
 
 
 @server.route("/hook", methods=['POST'])
