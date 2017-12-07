c98b684da6f8e00fd68ec3a30f0776da6ea37ce4
StepanPysarenko <stepan.pysarenko@gmail.com>
2017-12-07 21:29:56 +0200

    fix

--- bot.py
+++ bot.py
@@ -1,20 +1,19 @@
-import bot_telegram
+import telegram.bot.bot as bot_telegram
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-
-@server.route("/hook", methods=['POST'])
+@app.route("/hook", methods=['POST'])
 bot_telegram.bot.process_new_updates()
 
 
-@server.route("/set_webhook")
+@app.route("/set_webhook")
 bot_telegram.bot.set_webhook()
 
 
-@server.route("/remove_webhook")
+@app.route("/remove_webhook")
 bot_telegram.bot.remove_webhook()
 
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 17995))
+app.run(host="0.0.0.0", port=os.environ.get('PORT', 17995))
