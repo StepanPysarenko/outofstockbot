bdedda597c5bb03434b18b21134b1b6d22702148
StepanPysarenko <stepan.pysarenko@gmail.com>
2017-12-07 19:53:51 +0200

    fix

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ def help(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     # bot.reply_to(message, message.text)
-    bot.send_message(message.chat.id, message.text)
+    # bot.send_message(message.chat.id, message.text)
     bot.send_message(message.chat.id, 
     	json.dumps(message, sort_keys=True, indent=4, separators=(',', ': ')))
 
