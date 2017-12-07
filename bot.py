f4a098a1cb9a2ee8f4353019545f0fcaac77397c
StepanPysarenko <stepan.pysarenko@gmail.com>
2017-12-07 20:11:14 +0200

    fix

--- bot.py
+++ bot.py
@@ -1,49 +1,20 @@
-import telebot #pyTelegramBotAPI
-import os
-import json
+import bot_telegram
 from flask import Flask, request
 
+app = Flask(__name__)
 
-bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
-server = Flask(__name__)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, 'Need help?')
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.send_message(message.chat.id, message.text)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['location'])
-def echo_message(message):
-    bot.send_message(message.chat.id, 
-    	str(message.location.latitude) + ', ' + str(message.location.longitude))
 
 
 @server.route("/hook", methods=['POST'])
-def get_message():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+bot_telegram.bot.process_new_updates()
 
 
 @server.route("/set_webhook")
-def set_webhook():
-    bot.remove_webhook()  
-    return bot.set_webhook(url=os.environ.get('HOOK_URL')), 200
+bot_telegram.bot.set_webhook()
 
 
 @server.route("/remove_webhook")
-def remove_webhook():   
-    return bot.remove_webhook(), 200
+bot_telegram.bot.remove_webhook()
 
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 17995))
---
+++ bot_telegram.py
@@ -0,0 +1,41 @@
+import telebot
+import os
+
+bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Need help?')
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['location'])
+def echo_message(message):
+    bot.send_message(message.chat.id, 
+    	str(message.location.latitude) + ', ' + str(message.location.longitude))
+
+
+def process_new_updates():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+def set_webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=os.environ.get('HOOK_URL'))
+    return "!", 200
+
+
+def remove_webhook():
+    bot.remove_webhook()
+    return "!", 200
