6d3c804b458bd582cd5a95de03798edc6e7a61d1
StepanPysarenko <stepan.pysarenko@gmail.com>
2017-12-07 21:30:54 +0200

    fix

--- bot.py
+++ bot.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 
 @app.route("/hook", methods=['POST'])
-bot_telegram.bot.process_new_updates()
+bot_telegram.process_new_updates()
 
 
 @app.route("/set_webhook")
-bot_telegram.bot.set_webhook()
+bot_telegramset_webhook()
 
 
 @app.route("/remove_webhook")
-bot_telegram.bot.remove_webhook()
+bot_telegram.remove_webhook()
 
 
 app.run(host="0.0.0.0", port=os.environ.get('PORT', 17995))
