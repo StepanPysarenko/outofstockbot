51aa0fdd079b16ebd0b1c1e044cb6509b24ff163
StepanPysarenko <stepan.pysarenko@gmail.com>
2017-12-09 20:05:46 +0200

    update

--- telegram.py
+++ telegram.py
@@ -20,6 +20,12 @@ def help(message):
     bot.send_message(message.chat.id, 'Need help?')
 
 
+@bot.message_handler(commands=['oos'])
+def oos(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*[types.KeyboardButton(name) for name in ['Brand 1', 'Brand 2']])
+
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.send_message(message.chat.id, message.text)
@@ -27,8 +33,7 @@ def echo_message(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['location'])
 def echo_message(message):
-    db_services = DbServices()
-    db_services.create_item({
+    DbServices().create_item({
         'date': int(time.time()), 
         'latitude': message.location.latitude,
         'longitude': message.location.longitude
@@ -37,7 +42,7 @@ def echo_message(message):
     	str(message.location.latitude) + ', ' + str(message.location.longitude))
 
 
-@app_telegram.route("/telegram/hook", methods=['POST'])
+@app_telegram.route("/telegram/" + os.environ.get('TELEGRAM_BOT_TOKEN'), methods=['POST'])
 def webhook():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
@@ -46,7 +51,8 @@ def webhook():
 @app_telegram.route("/telegram/set_webhook")
 def set_webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=os.environ.get('TELEGRAM_HOOK_URL'))
+    bot.set_webhook(url=os.environ.get('BASE_URL') 
+        + "/telegram/" + os.environ.get('TELEGRAM_BOT_TOKEN'))
     return "!", 200
 
 
